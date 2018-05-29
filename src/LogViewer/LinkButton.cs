03293360922e67eeb273b857101eb1cbf16c30d5
cxwl3sxl <xinglin.sc@outlook.com>
2018-05-29 15:36:56 +0800

    遗漏的问题件

---
+++ src/LogViewer/LinkButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LogViewer
+{
+    public class LinkButton : Button
+    {
+        static LinkButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LinkButton),
+                new FrameworkPropertyMetadata(typeof(LinkButton)));
+        }
+
+        public static readonly DependencyProperty HighlightBrushProperty =
+            DependencyProperty.Register(nameof(HighlightBrush), typeof(Brush), typeof(LinkButton));
+
+        public Brush HighlightBrush
+        {
+            get => (Brush)GetValue(HighlightBrushProperty);
+            set => SetValue(HighlightBrushProperty, value);
+        }
+    }
+}
